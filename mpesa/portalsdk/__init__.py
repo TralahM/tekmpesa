898ce1d61271ae3b0d9901dbbbf5af688fc99f22
TralahM <musyoki.brian@tralahtek.com>
2020-11-30 14:26:07 +0300

    portalsdk notes module docstring
    Signed-off-by: TralahM <musyoki.brian@tralahtek.com>

--- mpesa/portalsdk/__init__.py
+++ mpesa/portalsdk/__init__.py
@@ -1,3 +1,26 @@
+"""
+.. note::
+    This module provides an abstraction over the API mechanisms of three submodules.
+    Namely:
+
+    - ``mpesa.tanzania``
+
+    - ``mpesa.ghana``
+
+    - ``mpesa.mozambique``
+
+    It Provides the following classes which are used internally in the
+    implementation of the respective API classes.
+
+    - ``mpesa.portalsdk.APIContext``
+
+    - ``mpesa.portalsdk.APIMethodType``
+
+    - ``mpesa.portalsdk.APIRequest``
+
+    - ``mpesa.portalsdk.APIResponse``
+
+"""
 from mpesa.portalsdk.api import APIContext
 from mpesa.portalsdk.api import APIMethodType
 from mpesa.portalsdk.api import APIRequest
